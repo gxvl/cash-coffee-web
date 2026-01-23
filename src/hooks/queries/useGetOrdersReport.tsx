5d36df9f60c09b6e6129fee6525b75e4ab0f3b21
Geraldo Gonçalves <geraldo.goncalves@ufrpe.br>
2026-01-23 08:25:50 -0300

    (fix): infer types

--- src/hooks/queries/useGetOrdersReport.tsx
+++ src/hooks/queries/useGetOrdersReport.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@tanstack/react-query";
 
 import type { OrdersReportEntity } from "@/common/entities/report";
@@ -12,26 +13,36 @@ export const useGetOrdersReport = (userId: string) => {
 
       // Map PascalCase to camelCase
       const mappedData: OrdersReportEntity = {
-        resumeCities: apiData.ResumeCities.map((item) => ({
-          city: item.City,
-          qtde: item.Qtde
-        })),
-        resumeAges: apiData.ResumeAges.map((item) => ({
-          range: item.Range,
-          qtde: item.Qtde
-        })),
-        resumeProducts: apiData.ResumeProducts.map((item) => ({
-          product: item.Product,
-          qtde: item.Qtde
-        })),
-        resumeDaysWeek: apiData.ResumeDaysWeek.map((item) => ({
-          dayWeek: item.DayWeek,
-          qtde: item.Qtde
-        })),
-        resumeGenders: apiData.ResumeGenders.map((item) => ({
-          gender: item.Gender,
-          perc: item.Perc
-        }))
+        resumeCities: apiData.ResumeCities.map(
+          (item: { City: any; Qtde: any }) => ({
+            city: item.City,
+            qtde: item.Qtde
+          })
+        ),
+        resumeAges: apiData.ResumeAges.map(
+          (item: { Range: any; Qtde: any }) => ({
+            range: item.Range,
+            qtde: item.Qtde
+          })
+        ),
+        resumeProducts: apiData.ResumeProducts.map(
+          (item: { Product: any; Qtde: any }) => ({
+            product: item.Product,
+            qtde: item.Qtde
+          })
+        ),
+        resumeDaysWeek: apiData.ResumeDaysWeek.map(
+          (item: { DayWeek: any; Qtde: any }) => ({
+            dayWeek: item.DayWeek,
+            qtde: item.Qtde
+          })
+        ),
+        resumeGenders: apiData.ResumeGenders.map(
+          (item: { Gender: any; Perc: any }) => ({
+            gender: item.Gender,
+            perc: item.Perc
+          })
+        )
       };
 
       return mappedData;
